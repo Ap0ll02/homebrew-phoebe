88355b0216207de60091e9947514ff0eb1c0fbfe
Apollo <jackratermann@gmail.com>
2025-01-05 21:45:39 -0800

    Trying it with correct url path

--- phoebe.rb
+++ phoebe.rb
@@ -1,7 +1,7 @@
 class Phoebe < Formula
   desc "A CLI tool for managing events"
-  homepage "https://github.com/Ap0ll02/PhoebeEvents"
-  url "https://github.com/Ap0ll02/PhoebeEvents/archive/refs/tags/v1.0.1.tar.gz"
+  homepage "https://github.com/Ap0ll02/phoebeevents"
+  url "https://github.com/Ap0ll02/phoebeevents/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "652c24f002060390848ed249b3cf1ad7ec22752e3dd6387a9653fe8e523e7d79"
   license "MIT"
 
